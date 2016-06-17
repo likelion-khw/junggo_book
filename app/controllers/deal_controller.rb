00de97378c252da1a2dc19ff9deabeddfcfb08e8
park ji yeon <yswldus99@naver.com>
2016-06-17 19:19:32 +0000

    book DB 생성,view처리, javascript 추가

--- app/controllers/deal_controller.rb
+++ app/controllers/deal_controller.rb
@@ -1,7 +1,39 @@
 class DealController < ApplicationController
   def sell_main
   end
-
+  
+  def sell_form
+    
+  end
+  
+  def sell_action
+    book = Book.new
+    book.book_title = params[:book_title]
+    book.book_writer = params[:book_writer]
+    book.book_price = params[:book_price]
+    book.book_category = params[:book_category]
+      
+    book.class_name = params[:class]
+    book.teacher = params[:teacher]
+      
+    book.out_state = params[:out_state]
+    book.in_state = params[:in_state]
+    book.saperate = params[:saperate]
+      
+    book.sell_price  = params[:sell_price]
+      
+    book.message_alert = params[:message_alert]
+    book.save
+    
+    redirect_to '/'
+  end
+  
   def buy_main
   end
+  
+  def view
+    @book = Book.find(params[:id])
+    @percent = ((1 - @book.sell_price.to_f/@book.book_price.to_f)*100).to_i
+  end
+  
 end
---
+++ db/migrate/20160617163630_create_books.rb
@@ -0,0 +1,23 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+      t.string :book_title
+      t.string :book_writer
+      t.integer :book_price
+      t.string :book_category
+      
+      t.string :class_name
+      t.string :teacher
+      
+      t.string :out_state
+      t.string :in_state
+      t.string :saperate
+      
+      t.integer :sell_price
+      
+      t.string :message_alert
+      
+      t.timestamps null: false
+    end
+  end
+end
